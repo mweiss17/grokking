3da342c68d219187d287caae51f2d0fc9d6a3be1
Martin Weiss <martin.clyde.weiss@gmail.com>
2021-10-25 17:25:09 -0400

    progress on training

--- scripts/01-train.py
+++ scripts/01-train.py
@@ -1,27 +1,63 @@
 import torch
+import sys
 from torch.utils.data import Dataset, DataLoader
 from grokking.dataset import ModularArithmetic, ModularArithmeticDataset, collate_fn
-from speedrun import BaseExperiment, WandBMixing, IOMixing
+from speedrun import BaseExperiment, WandBMixin, IOMixin
 
 
 class Trainer(BaseExperiment, WandBMixin, IOMixin):
-    WANDB_PROJECT = "grokking"
+    WANDB_PROJECT = "mila-grokking"
     WANDB_ENTITY = "mweiss10"
 
     def __init__(self):
         super(Trainer, self).__init__()
         self.auto_setup()
-        self.train_dataloader, self.valid_dataloader = self._build()
+        self.train_dataloader, self.valid_dataloader = self._build_dataset()
+        self.model = self._build_model()
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), self.get("lr"))
 
-    def _build(self):
+    def _build_dataset(self):
         ma = ModularArithmetic()
         train_ds = ModularArithmeticDataset(ma, train=True)
         valid_ds = ModularArithmeticDataset(ma, train=False)
-        batch_size = 512
-        train_dataloader = DataLoader(train_ds, shuffle=True, collate_fn=collate_fn, batch_size=batch_size)
-        valid_dataloader = DataLoader(valid_ds, shuffle=True, collate_fn=collate_fn, batch_size=batch_size)
+        train_dataloader = DataLoader(train_ds, shuffle=True, collate_fn=collate_fn, batch_size=self.get("batch_size"))
+        valid_dataloader = DataLoader(valid_ds, shuffle=True, collate_fn=collate_fn, batch_size=self.get("batch_size"))
         return train_dataloader, valid_dataloader
 
-#
-for step, (inputs, outputs) in enumerate(iter(train_dataloader)):
+    def _build_model(self):
+        pass
 
+    def run(self):
+        for step, (inputs, outputs) in enumerate(self.train_dataloader):
+            # Forward
+            preds = self.model(inputs)
+
+            # Compute loss
+            self.model.loss_fn(preds, outputs)
+
+            # Optimize
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+            # Record metrics
+
+            # Checkpoint
+
+    def save_checkpoint(self):
+        data = {"model": self.model.state_dict(), "optim": self.optimizer.state_dict()}
+        checkpoint_path = f"{self.experiment_directory}/Weights/model-{self.step}.pt"
+        print(f"checkpointing the model to {checkpoint_path}")
+        torch.save(data, checkpoint_path)
+
+    def load_checkpoint(self):
+        checkpoint_data = torch.load(self.get("checkpoint_path"))
+        self.model.load_state_dict(checkpoint_data["model"])
+        self.optimizer.load_state_dict(checkpoint_data["optim"])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        sys.argv.append("experiments/arithmetic-test")
+        sys.argv.append("--inherit")
+        sys.argv.append("templates/arithmetic")
+    Trainer().run()
--- src/grokking/dataset.py
+++ src/grokking/dataset.py
@@ -7,15 +7,15 @@ InputTensor = torch.Tensor
 # Tensor of size 1
 OutputTensor = torch.Tensor
 # 0 = op, 1 = '='
-NUM_SPECIAL_CHARS = 2
+NUM_SPECIAL_CHARS = 3
 
 class ModularArithmetic:
     # TODO: Clean this up https://pytorch.org/docs/stable/_modules/torch/utils/data/dataset.html#IterableDataset
     def __init__(self, train_data_fraction: float = 0.5):
         self.p: int = 97
         self.train_data_fraction = train_data_fraction
-        self.output_matrix = self.build_output_matrix()
         self.input_matrix = self.build_input_matrix()
+        self.output_matrix = self.build_output_matrix()
         self.mask = self.build_mask()
 
     def build_output_matrix(self):
@@ -26,14 +26,15 @@ class ModularArithmetic:
         return matrix
 
     def build_input_matrix(self):
-        matrix = torch.zeros(size=(self.p, self.p, 4), dtype=int)
+        matrix = torch.zeros(size=(self.p, self.p, 5), dtype=int)
         for i in range(self.p):
             for j in range(self.p):
                 # Increment both input values by two to make space for special tokens
-                matrix[i][j][0] = i + NUM_SPECIAL_CHARS
-                matrix[i][j][1] = 0  # special token for operation
-                matrix[i][j][2] = j + NUM_SPECIAL_CHARS
-                matrix[i][j][3] = 1  # special token for '='
+                matrix[i][j][0] = 0  # Start of Sequence token
+                matrix[i][j][1] = i + NUM_SPECIAL_CHARS
+                matrix[i][j][2] = 1  # special token for operation
+                matrix[i][j][3] = j + NUM_SPECIAL_CHARS
+                matrix[i][j][4] = 2  # special token for '='
         return matrix
 
     def build_mask(self):
@@ -65,10 +66,10 @@ def collate_fn(samples: List[Tuple[InputTensor, OutputTensor]]):
     return (inputs, outputs)
 
 
-if __name__ == '__main__':
-    ma = ModularArithmetic()
-    train_ds = ModularArithmeticDataset(ma, train=True)
-    valid_ds = ModularArithmeticDataset(ma, train=False)
-    batch_size = 512
-    train_dataloader = DataLoader(train_ds, shuffle=True, collate_fn=collate_fn, batch_size=batch_size)
-    print(next(iter(train_dataloader)))
+# if __name__ == '__main__':
+#     ma = ModularArithmetic()
+#     train_ds = ModularArithmeticDataset(ma, train=True)
+#     valid_ds = ModularArithmeticDataset(ma, train=False)
+#     batch_size = 512
+#     train_dataloader = DataLoader(train_ds, shuffle=True, collate_fn=collate_fn, batch_size=batch_size)
+#     print(next(iter(train_dataloader)))
