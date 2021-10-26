trains -- probably need to fix masking

scripts/01-train.py
@@ -1,6 +1,7 @@
 import torch
 import sys
 from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
 from grokking.dataset import ModularArithmetic, ModularArithmeticDataset, collate_fn
 from speedrun import BaseExperiment, WandBMixin, IOMixin
 from grokking.model import TransformerModel
@@ -12,6 +13,8 @@ class Trainer(BaseExperiment, WandBMixin, IOMixin):
     def __init__(self):
         super(Trainer, self).__init__()
         self.auto_setup()
+        if self.get("use_wandb"):
+            self.initialize_wandb()
         self.train_dataloader, self.valid_dataloader = self._build_dataset()
         self.device = torch.device('cpu')
         self.model = self._build_model()
@@ -31,21 +34,36 @@ class Trainer(BaseExperiment, WandBMixin, IOMixin):
         model = TransformerModel(self.get("ntokens"), self.get("emsize"), self.get("nhead"), self.get("d_hid"), self.get("nlayers"), self.get("dropout")).to(self.device)
         return model
 
+    def compute_accuracy(self, preds, labels):
+
+        correct = preds.argmax(1).eq(labels).sum() / labels.size()[0]
+        return correct
+
     def run(self):
-        for step, (inputs, outputs) in enumerate(self.train_dataloader):
-            # Forward
-            preds = self.model(inputs, torch.tensor([0]))
+        for epoch in range(self.get("n_epochs")):
+            for step, (inputs, labels) in enumerate(self.train_dataloader):
+                # Forward
+                preds = self.model(inputs)
+                correct = self.compute_accuracy(preds, labels)
 
-            # Compute loss
-            self.model.loss_fn(preds, outputs)
+                # Compute loss
+                loss = self.model.loss_fn(preds, labels)
+                loss.backward()
 
-            # Optimize
-            self.optimizer.step()
-            self.optimizer.zero_grad()
+                # Optimize
+                self.optimizer.step()
+                self.optimizer.zero_grad()
 
-            # Record metrics
+                self.next_step()
 
-            # Checkpoint
+                # Record metrics
+                if self.get("use_wandb"):
+                    self.wandb_log(**{"loss": loss.detach(), "step": self.step})
+                else:
+                    print(f'"loss": {loss.detach()}, "step": {self.step}, "% correct": {correct}')
+                # Checkpoint
+
+            self.next_epoch()
 
     def save_checkpoint(self):
         data = {"model": self.model.state_dict(), "optim": self.optimizer.state_dict()}

src/grokking/dataset.py
@@ -26,7 +26,7 @@ class ModularArithmetic:
         return matrix
 
     def build_input_matrix(self):
-        matrix = torch.zeros(size=(self.p, self.p, 5), dtype=int)
+        matrix = torch.zeros(size=(self.p, self.p, 6), dtype=int)
         for i in range(self.p):
             for j in range(self.p):
                 # Increment both input values by two to make space for special tokens
@@ -35,6 +35,7 @@ class ModularArithmetic:
                 matrix[i][j][2] = 1  # special token for operation
                 matrix[i][j][3] = j + NUM_SPECIAL_CHARS
                 matrix[i][j][4] = 2  # special token for '='
+                matrix[i][j][5] = self.operation(i, j) + NUM_SPECIAL_CHARS
         return matrix
 
     def build_mask(self):

src/grokking/model.py
@@ -30,33 +30,35 @@ class TransformerModel(nn.Module):
         self.decoder.bias.data.zero_()
         self.decoder.weight.data.uniform_(-initrange, initrange)
 
-    def forward(self, src: Tensor, memory: Tensor) -> Tensor:
+    def forward(self, src: Tensor) -> Tensor:
 
         # forward the GPT model
-        breakpoint()
-
-        token_embeddings = self.encoder(src) # each index maps to a (learnable) vector
-        position_embeddings = self.pos_emb[:, :t, :] # each position maps to a (learnable) vector
-        x = self.drop(token_embeddings + position_embeddings)
-        x = self.blocks(x)
-        x = self.ln_f(x)
-        logits = self.head(x)
-        
-
+        # breakpoint()
+        #
+        # token_embeddings = self.encoder(src) # each index maps to a (learnable) vector
+        # position_embeddings = self.pos_emb[:, :t, :] # each position maps to a (learnable) vector
+        # x = self.drop(token_embeddings + position_embeddings)
+        # x = self.blocks(x)
+        # x = self.ln_f(x)
+        # logits = self.head(x)
+        #
         src = self.encoder(src) * math.sqrt(self.d_model)
+        # tgt = self.encoder(torch.zeros((src.shape[0], 1), dtype=torch.long)) * math.sqrt(self.d_model)
         src = self.pos_encoder(src)
-        output = self.transformer_decoder(tgt, memory, tgt_mask, memory_mask)
-        
+        # tgt = torch.cat([src, tgt], axis=1)
+        output = self.transformer_decoder(src, src)
         output = self.decoder(output)
+        output = output[:, -1] # last token
         return output
 
+    def loss_fn(self, input, target):
+        loss = self.criterion(input, target)
+        return loss
 
 def generate_square_subsequent_mask(sz: int) -> Tensor:
     """Generates an upper-triangular matrix of -inf, with zeros on diag."""
     return torch.triu(torch.ones(sz, sz) * float('-inf'), diagonal=1)
 
-
-# In[19]:
 
 
 class PositionalEncoding(nn.Module):
@@ -81,6 +83,3 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 
-    def loss_fn(self, input, target):
-        loss = self.criterion(input, target)
-        return loss